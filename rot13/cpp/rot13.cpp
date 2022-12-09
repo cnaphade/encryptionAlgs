d154a01ae2b44ef3b35902f589750dc90912df2c
Chaitanya Naphade <chaitanya.naphade@gmail.com>
2022-12-08 18:35:24 -0800

    fidelity confirmed

--- rot13/cpp/rot13.cpp
+++ rot13/cpp/rot13.cpp
@@ -7,11 +7,28 @@ using namespace std;
 
 string rot13(string text, map<char, char> mapping) {
     string cipher_shift = "";
-    for (int i = 0; i <= text.length(); i++) {
+    for (int i = 0; i < text.length(); i++) {
         if (mapping.count(text[i])) cipher_shift += mapping[text[i]];
         else cipher_shift += text[i];
     }
     return cipher_shift;
+}
+
+void test_cipher() {
+    // check if encryption works
+    ifstream file_1("../OGtext.txt");
+    string text_1((std::istreambuf_iterator<char>(file_1)),
+                (std::istreambuf_iterator<char>()));
+    file_1.close();
+    ifstream file_2("../decrypted.txt");
+    string text_2((std::istreambuf_iterator<char>(file_2)),
+                (std::istreambuf_iterator<char>()));
+    file_2.close();
+    if (text_1 == text_2) cout << true << '\n';
+    else cout << false << '\n';
+
+    cout << text_1.length() << '\n';
+    cout << text_2.length() << '\n';
 }
 
 int main() {
@@ -47,4 +64,5 @@ int main() {
         decrypted.close();
     }
     
+    test_cipher();
 }
