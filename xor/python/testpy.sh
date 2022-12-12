d2ca917a7ca697ff5d6ed77342cd8983146cff9b
Chaitanya Naphade <chaitanya.naphade@gmail.com>
2022-12-12 15:00:16 -0800

    refactored python and updated testing

--- xor/python/testpy.sh
+++ xor/python/testpy.sh
@@ -3,17 +3,30 @@ arg="random"
 pass="password"
 if [ "$1" = $arg ]
 	then
-		openssl rand -base64 32 > ../OGtext.txt
-		pass=$(openssl rand -base64 12)
+		file_size=$(( RANDOM * 300))
+		pass_size=$(( $RANDOM % 50 + 1 ))
+		openssl rand -base64 $file_size > ../OGtext.txt
+		pass=$(openssl rand -base64 $pass_size)
+		echo "file size: " $file_size
 fi
 
-echo $pass
-python3 xor.py --choice e --password $pass
-python3 xor.py --choice d --password $pass
+echo "password: " $pass
+echo "password size: " ${#pass}
+python3 xor.py --choice e --password $pass --in_file $2 --out_file $3
+python3 xor.py --choice d --password $pass --in_file $3 --out_file $4
 
-if cmp -s "../OGtext.txt" "../decrypted.txt"
+if cmp -s $2 $4
 	then
-		echo Algorithm Works!
+		pass_size=$(( $RANDOM % 10 + 1 ))
+		echo "test pass size: " $pass_size
+		pass=$(openssl rand -base64 $pass_size)
+		python3 xor.py --choice d --password $pass --in_file $3 --out_file $4
+		if cmp -s $2 $4
+			then
+				echo Failure!
+			else
+				echo Algorithm Works!
+		fi
 	else
-		echo Files Differ!
+		echo Failure!
 fi
--- xor/python/xor.py
+++ xor/python/xor.py
@@ -1,17 +1,10 @@
 import os.path
 import argparse
 
-def generate_full_key(text, password):
-	key = ""
-	for i in range(len(text)):
-		key += password[i % len(password)]
-	return key
-
 def xor(text, password):
-	key = generate_full_key(text, password)
 	cipher_text = ""
 	for i in range(len(text)):
-		cipher_text += chr(ord(text[i]) ^ ord(key[i]))
+		cipher_text += chr(ord(text[i]) ^ ord(password[i % len(password)]))
 	return cipher_text
 
 def main():
@@ -19,23 +12,25 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--choice")
 	parser.add_argument("--password")
+	parser.add_argument("--in_file")
+	parser.add_argument("--out_file")
 	args = parser.parse_args()
 
 	if args.choice == 'e':
-		file = open(text_path + '/OGtext.txt', "rb")
+		file = open(args.in_file, "rb")
 		text = file.read()
 		file.close()
 
-		e_text = open(text_path + '/encrypted.txt', 'wb')
+		e_text = open(args.out_file, 'wb')
 		e_text.write(xor(text.decode(), args.password).encode())
 		e_text.close()
 
 	if args.choice == 'd':
-		file = open(text_path + '/encrypted.txt', "rb")
+		file = open(args.in_file, "rb")
 		text = file.read()
 		file.close()
 
-		d_text = open(text_path + '/decrypted.txt', 'wb')
+		d_text = open(args.out_file, 'wb')
 		d_text.write(xor(text.decode(), args.password).encode())
 		d_text.close()
 
